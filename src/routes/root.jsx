417ee441d58569545d9780449546dff4a9faf956
webkawsar <web.kawsarahmed@gmail.com>
2022-11-23 18:20:51 +0600

    Active Link Styling

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Form, Link, Outlet, redirect, useLoaderData } from "react-router-dom";
+import { Form, NavLink, Outlet, redirect, useLoaderData } from "react-router-dom";
 import { createContact, getContacts } from "../contacts";
 
 export async function action() {
@@ -40,7 +40,17 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -49,7 +59,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
