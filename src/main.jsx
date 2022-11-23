d313f422d01298819e5d5e28b65e5319635883ea
webkawsar <web.kawsarahmed@gmail.com>
2022-11-23 12:04:36 +0600

    handleing error

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
+import ErrorPage from "./error-page";
 import "./index.css";
+import Root from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
