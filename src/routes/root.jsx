44d36425c77a089b3667c72b2a56ace9633ecbd8
webkawsar <web.kawsarahmed@gmail.com>
2022-11-23 12:18:34 +0600

    added Nested Routes

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from "react-router-dom";
+
 export default function Root() {
     return (
       <>
@@ -29,15 +31,17 @@ export default function Root() {
           <nav>
             <ul>
               <li>
-                <a href={`contacts/1`}>Your Name</a>
+                <a href={`/contacts/1`}>Your Name</a>
               </li>
               <li>
-                <a href={`contacts/2`}>Your Friend</a>
+                <a href={`/contacts/2`}>Your Friend</a>
               </li>
             </ul>
           </nav>
         </div>
-        <div id="detail"></div>
+        <div id="detail">
+            <Outlet />
+        </div>
       </>
     );
   }
