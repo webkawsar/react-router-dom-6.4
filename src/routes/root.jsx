43bf105438678631a880240b85c414cb0dfa8d21
webkawsar <web.kawsarahmed@gmail.com>
2022-11-24 08:30:39 +0600

    GET Submissions with Client Side Routing1

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -15,7 +15,7 @@ export default function Root() {
         <div id="sidebar">
           <h1>React Router Contacts</h1>
           <div>
-            <form id="search-form" role="search">
+            <Form id="search-form" role="search">
               <input
                 id="q"
                 aria-label="Search contacts"
@@ -32,7 +32,7 @@ export default function Root() {
                 className="sr-only"
                 aria-live="polite"
               ></div>
-            </form>
+            </Form>
             <Form method="post">
               <button type="submit">New</button>
             </Form>
@@ -83,7 +83,9 @@ export default function Root() {
     );
   }
 
-  export async function loader() {
-    const contacts = await getContacts();
+  export async function loader({ request }) {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
     return { contacts };
   }
