c861603d6a4b93ab3d72e518ad9e71765f87abe8
webkawsar <web.kawsarahmed@gmail.com>
2022-11-24 19:39:35 +0600

    Adding Search Spinner

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -11,6 +11,7 @@ export default function Root() {
   const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
     const submit = useSubmit();
+    const searching = navigation.location && new URLSearchParams(navigation.location.search).has("q");
 
     useEffect(() => {
       document.getElementById("q").value = q;
@@ -24,6 +25,7 @@ export default function Root() {
             <Form id="search-form" role="search">
               <input
                 id="q"
+                className={searching ? "loading" : ""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
@@ -36,7 +38,7 @@ export default function Root() {
               <div
                 id="search-spinner"
                 aria-hidden
-                hidden={true}
+                hidden={!searching}
               />
               <div
                 className="sr-only"
